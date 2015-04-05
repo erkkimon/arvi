429cae859463b7cce85812224a7de72adbb248fd
Tom Himanen <tom.himanen@gmail.com>
2015-04-05 04:04:16 +0300

    New xml2json lib in test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,11 +3,12 @@ angular.module('starter.controllers', [])
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 })
 
-.controller('FeedsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Pena-Erkki veti itseään turpaan' },
-    { title: 'Ruotsi hävisi Suomelle 7–5' },
-    { title: 'Antti onkin ilmestyskirjan peto' },
-    { title: 'Orava pihalla – katso kuvat' }
-  ];
+.controller('FeedsCtrl', function($http, $scope) {
+//  $scope.playlists = [
+//    { title: 'Pena-Erkki veti itseään turpaan' },
+//    { title: 'Ruotsi hävisi Suomelle 7–5' },
+//    { title: 'Antti onkin ilmestyskirjan peto' },
+//    { title: 'Orava pihalla – katso kuvat' }
+//  ];
+
 });
