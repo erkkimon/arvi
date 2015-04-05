e38ee7ff4456c988770c86cf621d71525c15ddb6
Tom Himanen <tom.himanen@gmail.com>
2015-04-05 15:09:34 +0300

    News titles to list view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,20 +4,10 @@ angular.module('starter.controllers', [])
 })
 
 .controller('FeedsCtrl', function($http, $scope, Feeds) {
-//  $scope.playlists = [
-//    { title: 'Pena-Erkki veti itseään turpaan' },
-//    { title: 'Ruotsi hävisi Suomelle 7–5' },
-//    { title: 'Antti onkin ilmestyskirjan peto' },
-//    { title: 'Orava pihalla – katso kuvat' }
-//  ];
-
-feedItems = function(data) {
-	$scope.feedItems = data.rss.channel.item[5].title;
-					    }
 		         
-		    Feeds.get(feedItems);
-
-
-//	console.log(Feeds.get());	
+// Bind feed items to $scope
+Feeds.get(function(data) {
+	$scope.feedItems = data.rss.channel.item;
+});
 
 });
--- www/js/services.js
+++ www/js/services.js
@@ -6,17 +6,15 @@ angular.module('starter.services', [])
                 $http.get(
                     'http://yle.fi/uutiset/rss/paauutiset.rss',
                     {transformResponse:function(data) {
-                      // convert the data to JSON and provide
-                      // it to the success function below
+                        // convert to JSON for the success function below
                         var x2js = new X2JS();
                         var json = x2js.xml_str2json( data );
                         return json;
                         }
                     }
-                ).
-                success(function(data, status) {
-                    // send the converted data back
-                    // to the callback function
+                )
+                .success(function(data, status) {
+                    // send converted data back to callback
                     callback(data);
                 })
            }
