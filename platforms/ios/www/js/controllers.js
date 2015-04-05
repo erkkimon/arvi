b9111d174b2dfb6a558271c601d247974e2257eb
Tom Himanen <tom.himanen@gmail.com>
2015-04-06 01:03:12 +0300

    Nearly MVP, only visual stuff not implemented

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -4,10 +4,25 @@ angular.module('starter.controllers', [])
 })
 
 .controller('FeedsCtrl', function($http, $scope, Feeds) {
-		         
-	// Bind feed items to $scope
-	Feeds.get(function(data) {
-		$scope.feedItems = data.rss.channel.item;
+
+	var feedUrls = [
+		"http://yle.fi/uutiset/rss/paauutiset.rss", 
+		"http://www.iltalehti.fi/rss.xml",
+		"http://www.kouvolansanomat.fi/rss/uutiset.xml",
+		"http://www.hs.fi/rss/?osastot=kotimaa"	
+	];
+
+	var allFeedItems = [];
+	feedUrls.forEach(function(feedUrl) {
+		Feeds.get(feedUrl, function(data) {
+			//allFeedItems = data.rss.channel.item;
+			data.rss.channel["item"].forEach(function(feedItem) {
+				feedItem["timestamp"] = Date.parse(feedItem["pubDate"])/1000;
+				allFeedItems.push(feedItem);
+			});
+		});
 	});
+
+	$scope.feedItems = allFeedItems;
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,18 +7,22 @@ angular.module('starter.controllers', [])
 
 	var feedUrls = [
 		"http://yle.fi/uutiset/rss/paauutiset.rss", 
-		"http://www.iltalehti.fi/rss.xml"
+		"http://www.iltalehti.fi/rss.xml",
+		"http://www.kouvolansanomat.fi/rss/uutiset.xml",
+		"http://www.hs.fi/rss/?osastot=kotimaa"	
 	];
 
+	var allFeedItems = [];
 	feedUrls.forEach(function(feedUrl) {
 		Feeds.get(feedUrl, function(data) {
-			$scope.feedItems = data.rss.channel.item;
+			//allFeedItems = data.rss.channel.item;
+			data.rss.channel["item"].forEach(function(feedItem) {
+				feedItem["timestamp"] = Date.parse(feedItem["pubDate"])/1000;
+				allFeedItems.push(feedItem);
+			});
 		});
 	});
 
-	// Bind feed items to $scope
-	//Feeds.get(feedUrl, function(data) {
-	//	$scope.feedItems = data.rss.channel.item;
-	//});
-	
+	$scope.feedItems = allFeedItems;
+
 });
