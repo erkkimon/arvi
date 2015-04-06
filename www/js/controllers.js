ffb85f08225cc4f2c7b44aae2eb2fa4a09709223
Tom Himanen <tom.himanen@gmail.com>
2015-04-06 18:51:24 +0300

    Styling stuff done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -20,7 +20,7 @@ angular.module('starter.controllers', [])
 				feedItem["timestamp"] = Date.parse(feedItem["pubDate"])/1000;
 				if(!("enclosure" in feedItem)) {
 					feedItem["enclosure"] = [];
-					feedItem.enclosure["_url"] = "http://rantakuntoon.fi/wp-content/themes/rantakuntoon.fi/assets/img/preview-img.jpg";
+					feedItem.enclosure["_url"] = "/img/news-default-bg.png";
 				}
 				allFeedItems.push(feedItem);
 				//if(("_url" in feedItem.enclosure)) {
